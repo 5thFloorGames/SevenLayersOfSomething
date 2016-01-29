480e1bae3a09f1bddeeb0278f865fafdd70f685a
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2016-01-29 23:53:29 +0200

    basic gameplay done

--- SevenLayersOfSummoning/Assets/DemonShooting.cs
+++ SevenLayersOfSummoning/Assets/DemonShooting.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class DemonShooting : MonoBehaviour {
 
 	public GameObject bullet;
+	private KeyCode[] pattern = {KeyCode.Z, KeyCode.X, KeyCode.C, KeyCode.V};
+	private int comboPhase = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -12,11 +14,15 @@ public class DemonShooting : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown ("Fire2")) {
+		if (Input.GetKeyDown(pattern [comboPhase])) {
+			comboPhase++;
+		}
+		if (comboPhase == 4) {
 			GameObject firedBullet = (GameObject)Instantiate (bullet, transform.position, transform.rotation);
 			Rigidbody2D bulletrb = firedBullet.GetComponent<Rigidbody2D> ();
 			bulletrb.AddForce (transform.up * 200f);
-			Physics2D.IgnoreCollision(firedBullet.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+			Physics2D.IgnoreCollision (firedBullet.GetComponent<Collider2D> (), GetComponent<Collider2D> ());
+			comboPhase = 0;
 		}
 	}
 }
--- SevenLayersOfSummoning/Assets/ShamanShooting.cs
+++ SevenLayersOfSummoning/Assets/ShamanShooting.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class ShamanShooting : MonoBehaviour {
 
 	public GameObject bullet;
+	private KeyCode[] pattern = {KeyCode.M, KeyCode.Comma, KeyCode.Period, KeyCode.Minus};
+	private int comboPhase = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -12,11 +14,15 @@ public class ShamanShooting : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown ("Fire1")) {
+		if (Input.GetKeyDown((pattern [comboPhase]))) {
+			comboPhase++;
+		}
+		if (comboPhase == 4) {
 			GameObject firedBullet = (GameObject)Instantiate (bullet, transform.position, transform.rotation);
 			Rigidbody2D bulletrb = firedBullet.GetComponent<Rigidbody2D> ();
 			bulletrb.AddForce (transform.up * (-1) * 200f);
 			Physics2D.IgnoreCollision(firedBullet.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+			comboPhase = 0;
 		}
 	}
 }
