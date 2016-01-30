2be4f347a7126d10ef47e3d8a698f14ea232c3ea
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2016-01-30 20:09:52 +0200

    more music

--- SevenLayersOfSummoning/Assets/Scripts/ComboCreator.cs
+++ SevenLayersOfSummoning/Assets/Scripts/ComboCreator.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class ComboCreator : MonoBehaviour {
 
-	private KeyCode[] pattern = {KeyCode.Z, KeyCode.X, KeyCode.C, KeyCode.V};
+	private KeyCode[] pattern1 = {KeyCode.Joystick1Button0, KeyCode.Joystick1Button1, KeyCode.Joystick1Button2, KeyCode.Joystick1Button3};
+	private KeyCode[] pattern2 = {KeyCode.Joystick2Button0, KeyCode.Joystick2Button1, KeyCode.Joystick2Button2, KeyCode.Joystick2Button3};
 
 	// Use this for initialization
 	void Start () {
@@ -11,18 +12,20 @@ public class ComboCreator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		KeyCode[] pattern = newPattern();
-		string toPrint = "";
-		for (int i = 0; i < 4; i++) {
-			toPrint += pattern[i] + ", ";
-		}
-		//print (toPrint);
 	}
 
-	public KeyCode[] newPattern(){
-		KeyCode[] pattern = {KeyCode.Z, KeyCode.X, KeyCode.C, KeyCode.V};
+	public KeyCode[] newPatternPlayer1(){
+		KeyCode[] pattern = new KeyCode[4];
 		for (int i = 0; i < 4; i++) {
-			pattern[i] = pattern[Random.Range(0,4)];
+			pattern[i] = pattern1[Random.Range(0,4)];
+		}
+		return pattern;
+	}
+
+	public KeyCode[] newPatternPlayer2(){
+		KeyCode[] pattern = new KeyCode[4];
+		for (int i = 0; i < 4; i++) {
+			pattern[i] = pattern2[Random.Range(0,4)];
 		}
 		return pattern;
 	}
